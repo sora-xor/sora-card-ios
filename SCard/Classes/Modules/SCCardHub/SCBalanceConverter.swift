69259585d45dbab809652036775f451d91c69c29
soracard <soracard@gmail.com>
2024-01-15 12:25:33 +0300

    fix SCBalanceConverter

--- SCard/Classes/Modules/SCCardHub/SCBalanceConverter.swift
+++ SCard/Classes/Modules/SCCardHub/SCBalanceConverter.swift
@@ -1,9 +1,9 @@
 class SCBalanceConverter {
 
-    static let eurMinorUnitsCount = 100  // TODO: check minor units currency
+    static let eurMinorUnitsCount = 100.0  // TODO: check minor units currency
 
     static func formatedBalance(balance: Int) -> String {
-        let balanceDecimal = Decimal(balance / Self.eurMinorUnitsCount)
+        let balanceDecimal = Decimal(Double(balance) / Self.eurMinorUnitsCount)
         let balanceString = NumberFormatter.fiat.stringFromDecimal(balanceDecimal) ?? ""
         return "€\(balanceString)"
     }
